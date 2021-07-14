5a67a9f5d741bc9d941530ea835adfee0ad416db
Thet Win Htwe <thetwinhtwe@gmail.com>
2021-07-14 21:34:59 +0630

    Area Calculator

--- Day-3/main.py
+++ Day-3/main.py
@@ -9,5 +9,10 @@
 # print(name)
 # name=input(' Name:')
 # print('hello '+name)
-age=input('tell me your age:')
-print('your age is '+ age)
+# age=input('tell me your age:')
+# print('your age is '+ age)
+### area calculator ### (pi r**2)
+PI=3.142
+r=int(input('radius: ')) # '2'=> 2
+area=PI*r**2
+print(area)
