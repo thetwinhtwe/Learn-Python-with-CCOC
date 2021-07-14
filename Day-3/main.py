bb3abc20ef1de2cb2d78ad7e87b0899131a4e3d7
Thet Win Htwe <thetwinhtwe@gmail.com>
2021-07-14 21:18:06 +0630

    Day 3 input + hello

---
+++ Day-3/main.py
@@ -0,0 +1,11 @@
+# print('hello world') #print hello world to console
+# print('hello world') #print hello world to console
+# print('hello world') #print hello world to console
+# print('hello world') #print hello world to console
+# print('hello world') #print hello world to console
+# input(' Name:')
+# print(input(' Name:'))
+# name=input(' Name:')
+# print(name)
+name=input(' Name:')
+print('hello '+name)
